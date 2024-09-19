1aebcde54b517b75b0c56e482cd3c2f77bca8e31
Krists <kpudzens@gmail.com>
2024-09-19 14:30:04 +0300

    Win

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
 #include "Applications/Main/MainApplication.h"
 
 int main(int argc, char *argv[]) {
-
-    std::cout<<"lol"<<std::endl;
-
-    // MainApplication app;
-    // app.initialize("data/entityMap.json");
-    // app.loadEntitiesFromFile("data/scenes/test-bullet.json");
-    // app.run();
+    MainApplication app;
+    app.initialize("data/entityMap.json");
+    app.loadEntitiesFromFile("data/scenes/test-bullet.json");
+    app.run();
 
     return 0;
 }
