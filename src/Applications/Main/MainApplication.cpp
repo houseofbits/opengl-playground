8aa4ca270cd7ace6a3fd2d78c41ae9ea3b402350
Krists Pudzens <kpudzens@gmail.com>
2024-12-28 16:03:59 +0200

    Multithreading

--- src/Applications/Main/MainApplication.cpp
+++ src/Applications/Main/MainApplication.cpp
@@ -6,28 +6,17 @@
 #include "../../Modules/Behaviours/BehavioursModule.h"
 #include "../../Modules/Application/ApplicationModule.h"
 #include "../../Modules/Application/Events/SystemEvent.h"
-#include <fstream>
 
 MainApplication::MainApplication() : Application() {
 }
 
 void MainApplication::initialize(const std::string &entityDefinitionFileName) {
-    std::ifstream file(entityDefinitionFileName);
-    if (file.fail()) {
-        Log::error("Application::Application: Failed to read " + entityDefinitionFileName);
-        return;
-    }
-    auto json = nlohmann::json::parse(file);
-    m_EntityContext.deserializeEntityMap(json);
-
     m_EntityContext.registerModule<ApplicationModule>();
     m_EntityContext.registerModule<CommonModule>();
     m_EntityContext.registerModule<RendererModule>();
     m_EntityContext.registerModule<PhysicsModule>();
     m_EntityContext.registerModule<BehavioursModule>();
-
     m_EntityContext.initializeSystems(m_ResourceManager, m_EventManager);
-
     m_EventManager.queueEvent<SystemEvent>(SystemEvent::ENTITY_SYSTEMS_READY);
 }
 
