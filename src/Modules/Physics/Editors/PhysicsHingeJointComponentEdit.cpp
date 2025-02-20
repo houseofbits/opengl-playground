0f94c11c0e05d63ae43751b91479c54fe151f9e7
Krists Pudzens <kpudzens@gmail.com>
2025-02-20 08:24:23 +0200

    A

--- src/Modules/Physics/Editors/PhysicsHingeJointComponentEdit.cpp
+++ src/Modules/Physics/Editors/PhysicsHingeJointComponentEdit.cpp
@@ -11,18 +11,22 @@ void PhysicsHingeJointComponentEdit::processEditor() {
         return;
     }
 
-    EntityLinkedComponentEdit::processBasic<PhysicsBodyComponent>(
+    if (EntityLinkedComponentEdit::processBasic<PhysicsBodyComponent>(
         *m_editorSystem->m_EntityContext,
         m_component->m_targetEntityAName,
         "Attachment entity A##TRANSFORM_PARENT_ENTITY_NAME_A",
         "Self"
-    );
+    )) {
+        m_linkedTransformA = getTransformComponentByName(m_component->m_targetEntityAName);
+    }
 
-    EntityLinkedComponentEdit::processBasic<PhysicsBodyComponent>(
+    if (EntityLinkedComponentEdit::processBasic<PhysicsBodyComponent>(
         *m_editorSystem->m_EntityContext,
         m_component->m_targetEntityBName,
         "Attachment entity B##TRANSFORM_PARENT_ENTITY_NAME_B"
-    );
+    )) {
+        m_linkedTransformB = getTransformComponentByName(m_component->m_targetEntityBName);
+    }
 
     ImGui::InputFloat3("Attachment A", reinterpret_cast<float *>(&m_component->m_localAttachmentA));
     ImGui::InputFloat3("Axis A", reinterpret_cast<float *>(&m_component->m_axisA));
@@ -44,20 +48,95 @@ void PhysicsHingeJointComponentEdit::processEditor() {
 }
 
 PhysicsHingeJointComponentEdit::TransformOption PhysicsHingeJointComponentEdit::getTransformTargetOptions(int index) {
-    return {
-        "Common attachment point",
-        true,
-        true,
-        false,
-        ImGuizmo::TRANSLATE,
-        false,
-        true,
+    switch (index) {
+        case 1:
+            return {
+                "Attachment point A",
+                true,
+                true,
+                false,
+                ImGuizmo::TRANSLATE,
+                false,
+                true,
+            };
+        case 2:
+            return {
+                "Attachment point B",
+                true,
+                true,
+                false,
+                ImGuizmo::TRANSLATE,
+                false,
+                true,
+            };
+        default:
+            return {
+                "Common attachment point",
+                true,
+                true,
+                false,
+                ImGuizmo::TRANSLATE,
+                false,
+                true,
+            };
     };
 }
 
 glm::mat4 PhysicsHingeJointComponentEdit::getWorldSpaceTransform(int index) {
-    return {};
+    if (m_linkedTransformA == nullptr || m_linkedTransformB == nullptr) {
+        return {};
+    }
+
+    if (index == 2) {
+        auto m = glm::mat4(1.0);
+        m = glm::translate(m, m_component->m_localAttachmentB);
+
+        return m_linkedTransformB->m_transform * m;
+    }
+
+    auto m = glm::mat4(1.0);
+    m = glm::translate(m, m_component->m_localAttachmentA);
+
+    return m_linkedTransformA->m_transform * m;
 }
 
 void PhysicsHingeJointComponentEdit::setWorldSpaceTransform(glm::mat4 m, int index) const {
+    if (m_linkedTransformA == nullptr || m_linkedTransformB == nullptr) {
+        return;
+    }
+
+    if (index == 1) {
+        glm::mat4 localA = glm::inverse(m_linkedTransformA->m_transform) * m;
+        m_component->m_localAttachmentA = glm::vec3(localA[3]);
+
+        return;
+    }
+
+    if (index == 2) {
+        glm::mat4 localB = glm::inverse(m_linkedTransformB->m_transform) * m;
+        m_component->m_localAttachmentB = glm::vec3(localB[3]);
+
+        return;
+    }
+
+    glm::mat4 localA = glm::inverse(m_linkedTransformA->m_transform) * m;
+    glm::mat4 localB = glm::inverse(m_linkedTransformB->m_transform) * m;
+
+    m_component->m_localAttachmentA = glm::vec3(localA[3]);
+    m_component->m_localAttachmentB = glm::vec3(localB[3]);
+}
+
+void PhysicsHingeJointComponentEdit::handleEntitySelection(Entity &e, Component *c) {
+    ComponentEdit::handleEntitySelection(e, c);
+
+    m_linkedTransformA = getTransformComponentByName(m_component->m_targetEntityAName);
+    m_linkedTransformB = getTransformComponentByName(m_component->m_targetEntityBName);
+}
+
+TransformComponent *PhysicsHingeJointComponentEdit::getTransformComponentByName(const std::string &name) const {
+    if (name.empty()) {
+        return m_entity->getComponent<TransformComponent>();
+    }
+
+    return m_editorSystem->m_EntityContext->findEntityComponent<TransformComponent>(name);
 }
--- src/Modules/Physics/Editors/PhysicsHingeJointComponentEdit.h
+++ src/Modules/Physics/Editors/PhysicsHingeJointComponentEdit.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../../../SourceLibs/imgui/imgui.h"
+#include "../../Common/Components/TransformComponent.h"
 #include "../../Editor/Helpers/TargetEntityHelper.h"
 #include "../../Editor/UI/ComponentEdit.h"
 
@@ -18,8 +18,10 @@ public:
     void processEditor() override;
 
     int getNumTransformTargets() override {
-        return 1;
+        return 3;
     }
+
+    void handleEntitySelection(Entity &e, Component *c) override;
 
     TransformOption getTransformTargetOptions(int index) override;
 
@@ -27,5 +29,10 @@ public:
 
     void setWorldSpaceTransform(glm::mat4 m, int index) const override;
 
+private:
+    [[nodiscard]] TransformComponent* getTransformComponentByName(const std::string& name) const;
+
     glm::vec2 angularLimits{0, 0};
+    TransformComponent *m_linkedTransformA{nullptr};
+    TransformComponent *m_linkedTransformB{nullptr};
 };
