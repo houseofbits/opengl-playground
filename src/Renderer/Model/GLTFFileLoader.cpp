c334423d2cb8b5109b82de3e36a959ff589df8e5
Krists <kpudzens@gmail.com>
2025-09-15 07:44:41 +0300

    Win

--- src/Renderer/Model/GLTFFileLoader.cpp
+++ src/Renderer/Model/GLTFFileLoader.cpp
@@ -1,6 +1,7 @@
 #define TINYGLTF_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#define TINYGLTF_NO_EXTERNAL_IMAGE
 
 #include "GLTFFileLoader.h"
 #include "../../Core/Helper/Log.h"
